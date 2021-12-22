3a7afd8a5bc45352e23f54d8fbebe3189d670883
James Keicher <jamesdkeicher@gmail.com>
2021-12-22 12:34:44 -0800

    Add vote and subtract voteworking for each poll option. Reset form after submission

--- polls/polls.js
+++ polls/polls.js
@@ -3,6 +3,18 @@ const pollQuestionEl = document.getElementById('current-question');
 const firstOptionEl = document.getElementById('first-option');
 const secondOptionEl = document.getElementById('second-option');
 
+const option1Votes = document.getElementById('first-count');
+const option2Votes = document.getElementById('second-count');
+
+const upVote1 = document.getElementById('first-option-btn-plus');
+const downVote1 = document.getElementById('first-option-btn-minus');
+
+const upVote2 = document.getElementById('second-option-btn-plus');
+const downVote2 = document.getElementById('second-option-btn-minus');
+
+let option1VoteCount = 0;
+let option2VoteCount = 0;
+
 newPoll.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -14,8 +26,31 @@ newPoll.addEventListener('submit', (e) => {
     pollQuestionEl.textContent = question;
     firstOptionEl.textContent = optionOne;
     secondOptionEl.textContent = optionTwo;
+
+    option1Votes.textContent = option1VoteCount;
+    option2Votes.textContent = option2VoteCount;
+
+    newPoll.reset();
 });
 
+
+upVote1.addEventListener('click', () => {
+    option1VoteCount++;
+    option1Votes.textContent = option1VoteCount;
+});
+downVote1.addEventListener('click', () => {
+    option1VoteCount--;
+    option1Votes.textContent = option1VoteCount;
+});
+
+upVote2.addEventListener('click', () => {
+    option2VoteCount++;
+    option2Votes.textContent = option2VoteCount;
+});
+downVote2.addEventListener('click', () => {
+    option2VoteCount--;
+    option2Votes.textContent = option2VoteCount;
+});
 
 
 
