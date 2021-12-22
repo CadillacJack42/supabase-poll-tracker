a0a7fbda13f51974015ffe61d5c843d47ffb2b9d
James Keicher <jamesdkeicher@gmail.com>
2021-12-22 12:03:56 -0800

    Successfully adding users to db using sign up form

--- signup/signup.js
+++ signup/signup.js
@@ -1,1 +1,32 @@
+import { signUp } from '../utils/fetch-utils.js';
+
+
+
+
+const signUpForm = document.getElementById('sign-up');
+
+signUpForm.addEventListener('submit', async(e) => {
+    e.preventDefault();
+
+    const poll = new FormData(signUpForm);
+
+    const userEmail = poll.get('email');
+    const userPassword = poll.get('password');
+
+    // signup function
+    let user = await signUp(userEmail, userPassword);
+
+    console.log(user);
+
+    // location.href = '../signin';
+
+});
+
+
+
+
+
+
+
+
 console.log("I'm a console log on the sign up page");
---
+++ utils/fetch-utils.js
@@ -0,0 +1,12 @@
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDAxOTY4MywiZXhwIjoxOTU1NTk1NjgzfQ.0WT-gqj-qvV0wYfg0QdblxbkS4J4rIq0wf8BI3R45yc';
+const SUPABASE_URL = 'https://cmewyjgphfnmytfmmpjy.supabase.co';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export const signUp = async(email, password) => {
+    const res = await client.auth.signUp({
+        email,
+        password,
+    });
+    return res.user;
+};
